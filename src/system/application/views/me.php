9fa74b5d55cd4060db3f9ed99cb54cdd1aca6593
cerebrit <cerebrit@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-01-09 22:54:52 +0000

    Commit chart libraries

--- src/system/application/views/me.php
+++ src/system/application/views/me.php
@@ -1,3 +1,10 @@
 <div id='user_body'>
 <h1>Welcome to the Act As One web platform!</h1>
 </div>
+
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="900" height="300" id="Column3D" >
+         <param name="movie" value="/libraries/Column3D.swf" />
+         <param name="FlashVars" value="&dataURL=Data.xml&chartWidth=900&chartHeight=300">
+         <param name="quality" value="high" />
+         <embed src="/libraries/Column3D.swf" flashVars="&dataURL=Data.xml&chartWidth=900&chartHeight=300" quality="high" width="900" height="300" name="Column3D" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+</object>
