8906c5d8d767ac92c49bdb6ebe6dae63f6717ef1
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-01-14 21:50:23 +0000

    minor edits

--- src/system/application/models/module.php
+++ src/system/application/models/module.php
@@ -21,6 +21,12 @@ class Module extends Model {
   function get_view_data($viewid) {
     $query = "SELECT dataid,name FROM public.view_data AS t1, public.data AS t2 WHERE t1.viewid='$viewid' AND t2.id=dataid";
     $result = $this->db->query($query);
-    return $result->result_array();
+    $datas = $result->result_array();
+    foreach($datas AS $data) {
+      $query = "SELECT \"constraint\" FROM public.data_constraint AS t1, public.constraint AS t2 WHERE dataid='$data[dataid]' AND t2.id=constraintid";
+      $result = $this->db->query($query);
+      $constraints = $result->result_array();
+      print_r($constraints);
+    }
   }
 }
