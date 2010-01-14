cb2c047f6bf9805bd4f86fefc07ea4c192ccfe1a
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-01-14 22:55:18 +0000

    further updates to modules

---
+++ src/system/application/views/modules/bar_chart.php
@@ -0,0 +1,11 @@
+<?php
+array_keys($data);
+?>
+<div id="module">
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="700" height="300" id="Column3D" >
+<param name="movie" value="./system/application/libraries/Bar2D.swf" />
+<param name="FlashVars" value="&dataURL=/system/application/models/Data.xml&chartWidth=700&chartHeight=300">
+<param name="quality" value="high" />
+<embed src="./system/application/libraries/Bar2D.swf" flashVars="&dataURL=./system/application/models/Data.xml&chartWidth=700&chartHeight=300" quality="high" width="700" height="300" name="Column3D" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+</object>
+</div>
---
+++ src/system/application/views/user_body_start.php
@@ -0,0 +1,2 @@
+<div id='user_body'><!-- START USER BODY-->
+<h1>Welcome to the Act As One web platform!</h1>
