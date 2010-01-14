5cdae4717e350660a46b5474b639c32a97d253ab
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-01-14 21:11:55 +0000

    minor edit

--- src/system/application/models/module.php
+++ src/system/application/models/module.php
@@ -8,6 +8,7 @@ class Module extends Model {
 
   /* PARAMS: $userid - user to lookup
    *         $modid - module to lookup
+   * DESCRP: add module to user account
    */
   function add_user($userid,$modid) {
     $query = "INSERT INTO public.user_module (userid,modid) VALUES ($userid,$modid)";
@@ -18,7 +19,7 @@ class Module extends Model {
    * DESCRP: return an array of data for the view
    */
   function get_view_data($viewid) {
-    $query = "SELECT * FROM public.view_data AS t1, public.data AS t2 WHERE t1.viewid='$viewid'";
+    $query = "SELECT dataid,name FROM public.view_data AS t1, public.data AS t2 WHERE t1.viewid='$viewid' AND t2.id=dataid";
     $result = $this->db->query($query);
     return $result->result_array();
   }
