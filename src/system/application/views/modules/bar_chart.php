286dbf9b996aefafb884cc4772c533d76d35bec2
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-01-17 16:55:33 +0000

    inor edits, add user_body_stop.php

--- src/system/application/views/modules/bar_chart.php
+++ src/system/application/views/modules/bar_chart.php
@@ -1,5 +1,5 @@
 <?php
-$xml= "<chart caption='Financial Chart' xAxisName='Month' yAxisName='$' showValues='0' numberPrefix='' paletteColors='FF0000,0000FF' canvasbgColor='FFFFFF' canvasBorderColor='000000' canvasBorderThickness='2'>";
+$xml= "<chart caption='Financial Chart' xAxisName='Month' yAxisName='$' showValues='0' numberPrefix='$' canvasbgColor='FFFFFF' canvasBorderColor='000000' canvasBorderThickness='2'>";
 $colors = array("FF0000","0E2964");
 date_default_timezone_set('America/New_York');
 $keys = array_keys($data);
@@ -14,43 +14,29 @@ $i = 0;
 foreach($keys AS $index) {
   $xml.= "<dataset seriesName='$index'>";
   foreach($data[$index] AS $d) {
-    $xml.= "<set value='$d[value]' color='$colors[$i]'/>";
+    $xml.= "<set value='".abs($d['value'])."' color='$colors[$i]'/>";
   }
   $xml.="</dataset>";
   $i++;
 }
-//$xml.="<trendlines><line startValue='26000' color='91C728' displayValue='Target' showOnTop='1'/></trendlines><styles><definition><style name='CanvasAnim' type='animation' param='_xScale' start='0' duration='1' /></definition><application><apply toObject='Canvas' styles='CanvasAnim' /></application></styles>";
 $xml.= "</chart>";
-echo "<pre>$xml</pre>"
 ?>
 
-
 <div id="module">
-
-     <?php
-
-     //Create the chart - Column 3D Chart with data from strXML variable using dataXML method
-
-     echo renderChartHTML("./system/application/libraries/MSColumn2D.swf", "", $xml, "myNext", 600, 300, false); 
-
-     ?>
-
+<?php
+//Create the chart - Column 3D Chart with data from strXML variable using dataXML method
+echo renderChartHTML("./system/application/libraries/MSColumn2D.swf", "", $xml, "myNext", 600, 300, false); 
+?>
 </div>
 
 <div id="module">
-
-     <?php
-
-     //Create the chart - Column 3D Chart with data from strXML variable using dataXML method
-
-     echo renderChartHTML("./system/application/libraries/StackedColumn2D.swf", "", $xml, "myNext", 600, 300, false, false);
-
-     ?>
-
+<?php
+//Create the chart - Column 3D Chart with data from strXML variable using dataXML method
+echo renderChartHTML("./system/application/libraries/StackedColumn2D.swf", "", $xml, "myNext", 600, 300, false, false);
+?>
 </div>
 
 <!-- legacy method, should use PHP method above -->
-
 <div id="module">
 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/fl\
 ash/swflash.cab#version=8,0,0,0" width="700" height="300" id="MSColumn3D.swf" >
@@ -60,18 +46,13 @@ ash/swflash.cab#version=8,0,0,0" width="700" height="300" id="MSColumn3D.swf" >
 <embed src="./system/application/libraries/MSColumn3D.swf" flashVars="&dataXML=<?=$xml?>" quality="high" width="700" height="300" name="Column3D" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
 </object>
 </div>
-
 <!-- -->
 
 <div id="module">
-
-     <?php
-
-     //Create the chart - Column 3D Chart with data from strXML variable using dataXML method
-
-     echo renderChartHTML("./system/application/libraries/StackedColumn3D.swf", "", $xml, "myNext", 600, 300, false, false);
-
-     ?>
+<?php
+//Create the chart - Column 3D Chart with data from strXML variable using dataXML method
+echo renderChartHTML("./system/application/libraries/StackedColumn3D.swf", "", $xml, "myNext", 600, 300, false, false);
+?>
 
 </div>
 
