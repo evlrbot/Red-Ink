a86ea7bc3fe4612c04a2d6c5a9adb52b9cc10231
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-01-17 21:45:44 +0000

    updated barchar

--- src/system/application/views/modules/bar_chart.php
+++ src/system/application/views/modules/bar_chart.php
@@ -14,7 +14,7 @@ $i = 0;
 foreach($keys AS $index) {
   $xml.= "<dataset seriesName='$index'>";
   foreach($data[$index] AS $d) {
-    $xml.= "<set value='".abs($d['value'])."' color='$colors[$i]'/>";
+    $xml.= "<set value='".abs($d['value']/10.0)."' color='$colors[$i]'/>";
   }
   $xml.="</dataset>";
   $i++;
@@ -22,36 +22,10 @@ foreach($keys AS $index) {
 $xml.= "</chart>";
 ?>
 
-<div id="module">
-<?php
-//Create the chart - Column 3D Chart with data from strXML variable using dataXML method
-echo renderChartHTML("./system/application/libraries/MSColumn2D.swf", "", $xml, "myNext", 600, 300, false); 
-?>
-</div>
 
 <div id="module">
 <?php
-//Create the chart - Column 3D Chart with data from strXML variable using dataXML method
-echo renderChartHTML("./system/application/libraries/StackedColumn2D.swf", "", $xml, "myNext", 600, 300, false, false);
-?>
-</div>
-
-<!-- legacy method, should use PHP method above -->
-<div id="module">
-<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/fl\
-ash/swflash.cab#version=8,0,0,0" width="700" height="300" id="MSColumn3D.swf" >
-<param name="movie" value="./system/application/libraries/MSColumn3D.swf" />
-<param name="FlashVars" value="&dataXML=<?=$xml?>"
-<param name="quality" value="high" />
-<embed src="./system/application/libraries/MSColumn3D.swf" flashVars="&dataXML=<?=$xml?>" quality="high" width="700" height="300" name="Column3D" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
-</object>
-</div>
-<!-- -->
-
-<div id="module">
-<?php
-//Create the chart - Column 3D Chart with data from strXML variable using dataXML method
-echo renderChartHTML("./system/application/libraries/StackedColumn3D.swf", "", $xml, "myNext", 600, 300, false, false);
+echo renderChartHTML("./system/application/libraries/StackedColumn3D.swf", "", $xml, "myNext", 800, 400, false, false);
 ?>
 
 </div>
