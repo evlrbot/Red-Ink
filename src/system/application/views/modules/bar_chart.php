03de85267935b318fd7842974eb34844cd54a51d
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-01-21 18:01:31 +0000

    minor edits to graphs

--- src/system/application/views/modules/bar_chart.php
+++ src/system/application/views/modules/bar_chart.php
@@ -1,6 +1,6 @@
 <?php
 $xml= "<chart caption='".htmlentities($mod['name'])."' xAxisName='Month' yAxisName='$' showValues='0' numberPrefix='$' canvasbgColor='FFFFFF' canvasBorderColor='000000' canvasBorderThickness='2'>";
-$colors = array("FF0000","0E2964");
+$colors = array("FF0000","0E2964","AA0000","0000FF");
 date_default_timezone_set('America/New_York');
 $keys = array_keys($data);
 $xml.= "<categories>";
@@ -11,6 +11,7 @@ foreach($data[$keys[0]] AS $d) {
 $xml.="</categories>";
 
 $i = 0;
+
 foreach($keys AS $index) {
   $xml.= "<dataset seriesName='$index' color='$colors[$i]'>";
   foreach($data[$index] AS $d) {
