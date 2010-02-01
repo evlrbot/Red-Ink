e746ed8b1b1689947e3a733f6b3f8109a368b7eb
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-02-01 21:20:22 +0000

    minor business form mods

--- src/system/application/controllers/business.php
+++ src/system/application/controllers/business.php
@@ -7,6 +7,7 @@ class Business extends Controller {
     $this->auth->access();
     $this->load->model("user");
     $this->load->model("biz");
+    $this->load->model("module");
   }
 
   function index() {
@@ -21,12 +22,42 @@ class Business extends Controller {
   }
 
   function add() {
-    $data = $this->user->get_account($_SESSION['userid']);
-    $this->load->view('site_nav',$data);
-    $this->user->load_nav($_SESSION['userid']);
-    $this->load->view('user_body_start');
-    $this->load->view('register_business');
-    $this->load->view('user_body_stop');
-    $this->load->view('site_foot');
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+      // validate data from post    
+      $this->load->library('form_validation');   
+      $rules = array(
+           array('field'=>'name','label'=>'Name','rules'=>'required|alpha_dash'),
+		   array('field'=>'address1','label'=>'Address 1','rules'=>'required|alpha_dash'),
+		   array('field'=>'address2','label'=>'Address 2','rules'=>'required|alpha_dash'),
+		   array('field'=>'city','label'=>'City','rules'=>'required|alpha_dash'),
+   		   array('field'=>'state','label'=>'State','rules'=>'required|alpha'),
+		   array('field'=>'zipcode','label'=>'Zip Code','rules'=>'required|integer')		   
+		   );
+      $this->form_validation->set_rules($rules);
+      $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+      // if create fails produce error result
+      if($this->form_validation->run() == FALSE) {
+ 		//generate some error stuff
+      }      
+      else { // if not reload page with success message
+      // pass data from post to create method
+      ;
+      	$data = array(
+      	'name'=>$this->input->post('name'),
+      	'address1'=>$this->input->post('address1'),
+      	);
+      	if($this->biz->create($data)) {
+			$data = array('msg'=>'<p><em>Some new success text</em</p>');
+		}
+      	else {
+			$data = array('msg'=>'<p class="error">That business is already registered in our system.</p>');
+		}
+      }
+    }
+	$data = $this->user->get_account($_SESSION['userid']);
+	$this->load->view('site_nav',$data);
+	 $this->user->load_nav($_SESSION['userid']);
+	$this->load->view('register_business',$data);
+	$this->load->view('site_foot'); 
   }
 }
--- src/system/application/views/register_business.php
+++ src/system/application/views/register_business.php
@@ -1,18 +1,30 @@
-<?= form_open(site_url('bussiness/add'),array('id'=>'businessinfoform')); ?>
+<?= form_open(site_url('business/add'),array('id'=>'businessinfoform')); ?>
 
 <h1>Business Info</h1>
 
 <p><?= form_label('Name','name'); ?></p>
 <?= form_error('name'); ?>
-<?= form_input(array('id'=>'name','name'=>'name','value'=>"")); ?>
+<?= form_input(array('id'=>'name','name'=>'name','value'=>"",'size'=>'50','style'=>'width:50%')); ?>
 
 <p><?= form_label('Address 1','address1'); ?></p>
 <?= form_error('address1'); ?>
-<?= form_input(array('id'=>'address1','name'=>'address1','value'=>"")); ?>
+<?= form_input(array('id'=>'address1','name'=>'address1','value'=>"",'size'=>'50','style'=>'width:50%')); ?>
 
 <p><?= form_label('Address 2','address2'); ?></p>
 <?= form_error('address2'); ?>
-<?= form_input(array('id'=>'address2','name'=>'address2','value'=>"")); ?>
+<?= form_input(array('id'=>'address2','name'=>'address2','value'=>"",'size'=>'50','style'=>'width:50%')); ?>
+
+<p><?= form_label('City','city'); ?></p>
+<?= form_error('city'); ?>
+<?= form_input(array('id'=>'city','name'=>'city','value'=>"",'size'=>'50','style'=>'width:50%')); ?>
+
+<p><?= form_label('State','state'); ?></p>
+<?= form_error('state'); ?>
+<?= form_input(array('id'=>'state','name'=>'state','size'=>'50','style'=>'width:50%')); ?>
+
+<p><?= form_label('Zip Code','zipcode'); ?>
+<?= form_error('zipcode'); ?>
+<?= form_input(array('id'=>'zipcode','name'=>'zipcode','size'=>'50','style'=>'width:50%')); ?>
 
 <p><?= form_submit(array('id'=>'submit','value'=>'Submit')); ?></p>
 
