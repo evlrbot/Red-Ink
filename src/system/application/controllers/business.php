4ca50ae8cdd696ce5ae1e44f58614e5730e268e2
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-02-02 18:01:03 +0000

    updates to biz reg code

--- src/system/application/controllers/business.php
+++ src/system/application/controllers/business.php
@@ -22,42 +22,53 @@ class Business extends Controller {
   }
 
   function add() {
+    $data = "";
     if($_SERVER['REQUEST_METHOD'] == "POST") {
-      // validate data from post    
       $this->load->library('form_validation');   
       $rules = array(
-           array('field'=>'name','label'=>'Name','rules'=>'required|alpha_dash'),
-		   array('field'=>'address1','label'=>'Address 1','rules'=>'required|alpha_dash'),
-		   array('field'=>'address2','label'=>'Address 2','rules'=>'required|alpha_dash'),
-		   array('field'=>'city','label'=>'City','rules'=>'required|alpha_dash'),
-   		   array('field'=>'state','label'=>'State','rules'=>'required|alpha'),
-		   array('field'=>'zipcode','label'=>'Zip Code','rules'=>'required|integer')		   
-		   );
+		     array('field'=>'name','label'=>'Name','rules'=>'required|alpha_dash'),
+		     array('field'=>'address1','label'=>'Address 1','rules'=>'required|alpha_dash'),
+		     array('field'=>'address2','label'=>'Address 2','rules'=>'alpha_dash'),
+		     array('field'=>'city','label'=>'City','rules'=>'required|alpha_dash'),
+		     array('field'=>'state','label'=>'State','rules'=>'required|alpha'),
+		     array('field'=>'zipcode1','label'=>'Zip Code','rules'=>'required|integer|max_length[5]'),
+		     array('field'=>'zipcode2','label'=>'Zip Code','rules'=>'integer|max_length[4]')		   
+		     );
       $this->form_validation->set_rules($rules);
       $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
-      // if create fails produce error result
-      if($this->form_validation->run() == FALSE) {
- 		//generate some error stuff
-      }      
-      else { // if not reload page with success message
-      // pass data from post to create method
-      ;
+      if(!$this->form_validation->run() == FALSE) {
       	$data = array(
-      	'name'=>$this->input->post('name'),
-      	'address1'=>$this->input->post('address1'),
-      	);
+		      'name'=>$this->input->post('name'),
+		      'address1'=>$this->input->post('address1'),
+		      'address2'=>$this->input->post('address2'),
+		      'city'=>$this->input->post('city'),
+		      'state'=>$this->input->post('state'),
+		      'zip'=>$this->input->post('zipcode1').'-'.$this->input->post('zipcode2')
+		      );
       	if($this->biz->create($data)) {
-			$data = array('msg'=>'<p><em>Some new success text</em</p>');
-		}
+	  $data = array('msg'=>'<p><em>The business has been successfully added.</em</p>');
+	}
       	else {
-			$data = array('msg'=>'<p class="error">That business is already registered in our system.</p>');
-		}
+	  $data = array('msg'=>'<p class="error">That business is already registered in our system.</p>');
+	}
       }
     }
-	$data = $this->user->get_account($_SESSION['userid']);
-	$this->load->view('site_nav',$data);
-	 $this->user->load_nav($_SESSION['userid']);
-	$this->load->view('register_business',$data);
-	$this->load->view('site_foot'); 
+    $user_data = $this->user->get_account($_SESSION['userid']);
+    $this->load->view('site_nav',$user_data);
+    $this->user->load_nav($_SESSION['userid']);
+    $this->load->view('user_body_start'); 
+    $this->load->view('register_business',$data);
+    $this->load->view('user_body_stop');
+    $this->load->view('site_foot'); 
+  }
+  
+  function delete($id) {
+    $this->biz->delete($id);
+    redirect('/business/index');
+  }
+
+  function deactivate($id) {
+    $this->biz->deactivate($id);
+    redirect('/business/index');
   }
 }
