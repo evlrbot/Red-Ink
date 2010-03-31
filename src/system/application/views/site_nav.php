658bf9de17e9215feeea4361371c4925de9e7c72
cerebrit <cerebrit@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-03-31 17:33:29 +0000

    Fix home link in site nav

--- src/system/application/views/site_nav.php
+++ src/system/application/views/site_nav.php
@@ -15,7 +15,7 @@ $(document).ready(function(){});
 <div id="site_nav">
 <span class="sitelinks">
 <em>Millipede</em> |
-<a href="#">Home</a>
+<a href="/me">Home</a>
 <a href="http://www.make-them-think.org/Blog/Index" target="_blank">Blog</a>
 <a href="http://www.make-them-think.org/Help/Index" target="_blank">Help</a>  
 <a href="/campaign/index">Campaigns</a>
