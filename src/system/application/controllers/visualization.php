d3decb7e2bd217274e3081be702aee777c57ed09
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-04-09 19:18:42 +0000

    refactoring visualization code

--- src/system/application/controllers/visualization.php
+++ src/system/application/controllers/visualization.php
@@ -38,116 +38,69 @@ class Visualization extends Controller {
   }
 
   function edit($modid,$modvizid) {
-
- 	$data_sets= $this->module->get_data_sets($modid);	
-
+    $data_sets= $this->module->get_data_sets($modid);	
     if($_SERVER['REQUEST_METHOD'] == "POST") {
-    	
-    	// these need to be moved into model and optimized
-    	
-    	$q= "DELETE FROM public.mod_viz_data WHERE modvizid=$modvizid";
-    	$this->db->query($q);
-    	
-    	foreach($data_sets as $d) {	
-    	    	
-	    	if($moddataid = $this->db->escape($this->input->post($d['dataid']))) {
-	    	
-	    		// needs optimization
-	    		$moddataid_color= $d['dataid'] . "_color";
-	    		$moddataid_color= $_POST[$moddataid_color];
-	    	
-				$q= "INSERT INTO public.mod_viz_data (modid, modvizid, moddataid, moddataid_color) VALUES ($modid, $modvizid, $moddataid, '$moddataid_color')";
-				$this->db->query($q);
-			}
-			
-    	}
-    	
-    	$viz_name= $this->db->escape($this->input->post('viz_name_field'));
-    	
-			$q= "UPDATE public.module_visualization SET viz_name=$viz_name WHERE id= $modvizid";
-			$this->db->query($q);
-		
-    	$viz_stacked= $this->db->escape($this->input->post('viz_stacked_field'));
-    	
-			$q= "UPDATE public.module_visualization SET stacked= $viz_stacked WHERE id= $modvizid";
-			$this->db->query($q);
-    	
-		if($this->db->escape($this->input->post('submit2'))) {
-		
-			$redirect= "/campaign/edit/$modid";
-		
-			redirect($redirect);
-		}
-    }
-
-    //var_dump($_POST);
-	
-	$user= $this->user->get_account($_SESSION['userid']);
-	$userid= $_SESSION['userid'];
-
-	$data_set_results= $this->module->get_data_sets_results($data_sets, $userid);
-
-	$template = $this->module->get_template($modvizid);
-	
-	$mod= $this->module->get_module($modid);
-	
-	$dataids= $this->module->get_modviz_datasets($modid, $modvizid);
-
-/*	load viz and chart data  */
-
-    $viz = $this->module->get_visualizations($modid, $modvizid);
-	
-	$viz_datasets= $this->viz->load_viz_datasets($dataids);
-	
-	$viz_data= $this->viz->load_viz_data($viz_datasets);
-	
-	$chart_name= $this->module->get_module($modid);
-	$chart_name= $chart_name['name'];	
-	
-	$xml= $this->viz->format_xml($viz_data, $dataids, $chart_name);
-	
-	$chart_data= array("viz"=>"", "xml"=>$xml, "viz_data"=>$viz_data);
-
-/*  */
-
-	$i= 0;
-
-	while($i< count($data_sets)) {
-    
-    	$d= $data_sets[$i]["dataid"]; 
-    
-		$data_sets[$i]['checked']= '';
-		
-		foreach($dataids as $did) {
-					
-			if($d== $did["moddataid"]) {
-			
-				$data_sets[$i]['checked']= 'checked';
-			}
-		}
-		
-		$i++;
+      // these need to be moved into model and optimized
+      $q= "DELETE FROM public.mod_viz_data WHERE modvizid=$modvizid";
+      $this->db->query($q);
+      foreach($data_sets as $d) {	
+	if($moddataid = $this->db->escape($this->input->post($d['dataid']))) {
+	  // needs optimization
+	  $moddataid_color= $d['dataid'] . "_color";
+	  $moddataid_color= $_POST[$moddataid_color];
+	  $q= "INSERT INTO public.mod_viz_data (modid, modvizid, moddataid, moddataid_color) VALUES ($modid, $modvizid, $moddataid, '$moddataid_color')";
+	  $this->db->query($q);
 	}
+      }
+      $viz_name= $this->db->escape($this->input->post('viz_name_field'));
+      $q= "UPDATE public.module_visualization SET viz_name=$viz_name WHERE id= $modvizid";
+      $this->db->query($q);
+      $viz_stacked= $this->db->escape($this->input->post('viz_stacked_field'));
+      $q= "UPDATE public.module_visualization SET stacked= $viz_stacked WHERE id= $modvizid";
+      $this->db->query($q);
+      if($this->db->escape($this->input->post('submit2'))) {
+	$redirect= "/campaign/edit/$modid";
+	redirect($redirect);
+      }
+    }
+    $user = $this->user->get_account($_SESSION['userid']);
+    $userid = $_SESSION['userid'];
+    $data_set_results= $this->module->get_data_sets_results($data_sets, $userid); // return the time series data for the dataset
+    $template = $this->module->get_template($modvizid);
+    $mod = $this->module->get_module($modid);
+    $dataids = $this->module->get_modviz_datasets($modid, $modvizid);
+
+    /*	load viz and chart data  */
+    $viz = $this->module->get_visualizations($modid, $modvizid); // get the info for this vis for this module
+    $viz_datasets = $this->viz->load_viz_datasets($dataids);     // for given dataset ids, run their queries, and return the results
+    $viz_data = $this->viz->load_viz_data($viz_datasets);        // this function is redundant
+    $xml = $this->viz->format_xml($viz_data, $dataids, $mod['name']);
+    $chart_data = array("viz"=>"", "xml"=>$xml, "viz_data"=>$viz_data);
+
+    // SET THE ACTIVE DATASETS FOR THIS VISUALIZATION FOR THIS MODULE
+    for($i=0; $i<count($data_sets);$i++) {
+      foreach($dataids as $id) {
+	$data_sets[$i]['checked'] = ($data_sets[$i]["dataid"] == $id["moddataid"]) ? 'checked' : '';
+      }
+    }
     
     // need to go through and see which are needed
     
     $data['xml'] = $xml;
-	$data['modid'] = $modid;
+    $data['modid'] = $modid;
     $data['user'] = $user;
-	$data['data_sets'] = $data_sets;
-	$data['template'] = $template;
+    $data['data_sets'] = $data_sets;
+    $data['template'] = $template;
     $data['mod'] = $mod;
-	$data['data_set_results'] = $data_set_results;
-	$data['modvizid']= $modvizid;
-	$data['dataids']= $dataids;
-	$data['chart_data']= $chart_data;
-	$data['viz']= $viz;
-
+    $data['data_set_results'] = $data_set_results;
+    $data['modvizid'] = $modvizid;
+    $data['dataids'] = $dataids;
+    $data['chart_data'] = $chart_data;
+    $data['viz'] = $viz;
+    
     $this->load->view('site_nav',$data['user']);	
     $this->user->load_nav($userid);    
     $this->load->view('user_body_start');
-
-    
     $this->load->view('modvizdata',$data);
     $this->load->view('user_body_stop');
     $this->load->view('site_foot');   
--- src/system/application/views/modvizdata.php
+++ src/system/application/views/modvizdata.php
@@ -1,68 +1,42 @@
 <div id="module">
-
 <?php
-
-	$xml= $chart_data['xml'];
-
+$xml= $chart_data['xml'];
 if(count($dataids) > 1) {
-
-	if($viz['viz_stacked']) {
-	
-		echo renderChartHTML("/system/application/libraries/$template[stacked]", "", "$xml", "myNext", 700, 300, false, false);
-	}
-	
-	else {
-	
-		echo renderChartHTML("/system/application/libraries/$template[multidata]", "", "$xml", "myNext", 700, 300, false, false);	
-	}
+  if($viz['viz_stacked']) {
+    echo renderChartHTML("/system/application/libraries/$template[stacked]", "", "$xml", "myNext", 700, 300, false, false);
+  }  
+  else {
+    echo renderChartHTML("/system/application/libraries/$template[multidata]", "", "$xml", "myNext", 700, 300, false, false);	
+  }
 }
 else {
-
-	echo renderChartHTML("/system/application/libraries/$template[template]", "", "$xml", "myNext", 700, 300, false, false);
+  echo renderChartHTML("/system/application/libraries/$template[template]", "", "$xml", "myNext", 700, 300, false, false);
 }
-
 ?>
-
 </div>
 
 <?php
-
-echo "<table id='list' border='0' cellpadding='10' cellspacing='2'>";
-echo "<thead><tr><td>Label</td><td>Query</td><td>Color</td></tr></thead><tbody>";
-
-echo form_open(site_url("visualization/edit/$modid/$modvizid"));
-
-echo "<div id='viz_name'><label for='viz_name_field'>Viz Name</label><input name= 'viz_name_field' value='$viz[viz_name]' id='viz_name_field'></div>";
-
+echo form_open(site_url("visualization/edit/$modid/$modvizid"),array('id'=>'bigform'));
+echo "<div id='viz_name'><label for='viz_name_field'>Visualization Label</label><input name='viz_name_field' value='$viz[viz_name]' id='viz_name_field'></div>\n";
 if(count($dataids) > 1) {
-
-	echo "<div id='viz_stacked'><label for='viz_stacked_field'>Stack Viz?</label><input name= 'viz_stacked_field' type='checkbox' $viz[viz_stacked] id='viz_stacked_field' value='checked'></div>";
+  echo "<div id='viz_stacked'><label for='viz_stacked_field'>Stack Viz?</label><input name= 'viz_stacked_field' type='checkbox' $viz[viz_stacked] id='viz_stacked_field' value='checked'></div>\n";
 }
-			
-echo "<table>";
+echo "<table id='list' border='0' cellpadding='10' cellspacing='2'>\n";
+echo "<thead><tr><td>Active</td><td>Label</td><td>Query</td><td>Color</td></tr></thead><tbody>\n";
 echo "<tr>";
-
-foreach($data_sets as $d) {
-	
-	echo "<td><input name= '" . $d['dataid'] . "' value='" . $d['dataid'] ."' type='checkbox' ". $d['checked'] . "></td><td>" . $d['name'] . "</td><td>" . $d['query'] . "</td>";
-	echo "<td><select name='" . $d['dataid'] . "_color'><option value='random'>Random</option><option value='0000FF'>Blue</option><option value='FF0000'>Red</option><option value='F7FF00'>Yellow</option><option value='00FF00'>Green</option><option value='FF00DD'>Purple</option><option value='FF8F00'>Orange</option></select></td>";
-	echo "</tr>";
+foreach($data_sets as $d) {	
+  echo "<td><input name= '" . $d['dataid'] . "' value='" . $d['dataid'] ."' type='checkbox' ". $d['checked'] . "></td><td>" . $d['name'] . "</td><td>" . $d['query'] . "</td>\n";
+  echo "<td><select name='" . $d['dataid'] . "_color'><option value='random'>Random</option><option value='0000FF'>Blue</option><option value='FF0000'>Red</option><option value='F7FF00'>Yellow</option><option value='00FF00'>Green</option><option value='FF00DD'>Purple</option><option value='FF8F00'>Orange</option></select></td>\n";
+  echo "</tr>\n";
 }
-
-echo "</table>";
-
 ?>
-
-<p style="text-align: center"><?= form_submit(array('id'=>'submit','value'=>'Save')); ?><?= form_button(array('name'=>'submit2', 'id'=>'submit2','value'=>'submit2', 'type'=>'submit'), 'Save and Go Back'); ?></p>
-
-<?= form_close(); ?>
+<tfoot><tr><td colspan='4'>
+<?=form_submit(array('id'=>'submit','value'=>'Save')); ?>
+<?=form_submit(array('id'=>'submit2','value'=>'Save And Return','name'=>'submit2')); ?>
+</td></tr></tfoot>
 
 <?php
-
-$count = 0;
-
 echo "</tbody></table>";
-
+echo form_close();
 ?>
-
 </table>
