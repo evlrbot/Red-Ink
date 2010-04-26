4b9d15c9ce9ed0a33d1fdc1dac5b955dfc992ccf
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-04-26 16:07:50 +0000

    ditto

---
+++ src/system/application/views/site_head.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title><?php echo isset($webpagename) ? $webpagename : "RED INK";?></title>
+<style type="text/css" media="all">
+@import url("/system/application/css/mainstyle.css");
+@import url("http://www.make-them-think.org/pub/skins/redink/pmwiki.css");
+</style>
+<script language="javascript" src="/system/application/js/jquery-1.3.2.min.js" ></script>
+<script language="javascript" src="/system/application/libraries/jquery.flot.min.js" ></script>
+<script language="javascript" src="/system/application/js/jquery.form.js" ></script>
+<script language="javascript">
+$(document).ready(function(){});
+</script>
+</head>
+<body>
