e9460a4b6e074376824629becfd62e1f6fb13c6f
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-06-28 18:57:21 +0000

    minor debug edit

--- src/system/application/models/module.php
+++ src/system/application/models/module.php
@@ -229,7 +229,6 @@ class Module extends Model {
     // get average spend/visit
 
     // GET MODULE DATA
-    echo "<p>$modid</p>";
     $data['module'] = $this->get_module($modid);
 
     // GET INDIVIDUAL MEMBER DATA
