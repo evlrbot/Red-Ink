ca7ccd7cb8e3b98ca6d41f56bc41b1e77f8ea87c
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-07-13 16:45:25 +0000

    updated table

--- src/system/application/modules/tabular/views/table.php
+++ src/system/application/modules/tabular/views/table.php
@@ -2,15 +2,16 @@
 <h2>My Transactions</h2>
 <table id='tabular'>
 <?php 
+if (sizeof($transactions)==0) 
+   echo "<p style='background-color:darkred;color:white;font-size:20px'>You currently have no transactions.</p>";
+else{
 echo "<thead><tr><td>Created</td><td>$$$</td><td>Memo</td><td>Merchant</td></tr></thead><tbody>\n";
 $i=0;
 foreach ($transactions AS $t) {
-// testing loop
- echo "h3ll0";
  $rowstyle = ($i++%2==0) ? "row1" : "row2";
   $t["amount"] = $t["amount"]/100;
   echo "<tr class='$rowstyle'><td>$t[created]</td><td>\$$t[amount]&nbsp;</td><td>$t[memo]&nbsp;</td><td>$t[merchant]</td></tr>\n";
-}
+}}
 ?>
 </tbody></table>
 </div>
