4fc6d5025b48fc0239308f54788e9fe100af46cf
rotoole <rotoole@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-07-13 15:47:58 +0000

    debugging table

--- src/system/application/modules/tabular/views/table.php
+++ src/system/application/modules/tabular/views/table.php
@@ -5,7 +5,9 @@
 echo "<thead><tr><td>Created</td><td>$$$</td><td>Memo</td><td>Merchant</td></tr></thead><tbody>\n";
 $i=0;
 foreach ($transactions AS $t) {
-  $rowstyle = ($i++%2==0) ? "row1" : "row2";
+// testing loop
+ echo "h3ll0";
+ $rowstyle = ($i++%2==0) ? "row1" : "row2";
   $t["amount"] = $t["amount"]/100;
   echo "<tr class='$rowstyle'><td>$t[created]</td><td>\$$t[amount]&nbsp;</td><td>$t[memo]&nbsp;</td><td>$t[merchant]</td></tr>\n";
 }
