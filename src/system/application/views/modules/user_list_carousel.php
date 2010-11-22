d5032dcc50e873a4646c2d49a1ba02541e6701af
scott <scott@81e7e2fb-d0a3-44d8-82cd-12c069ed030c>
2010-11-22 22:43:23 +0000

    carousel list

---
+++ src/system/application/views/modules/user_list_carousel.php
@@ -0,0 +1,8 @@
+<?php
+echo "<ul id='user_modules_carousel'>";
+foreach($modules as $m) {
+  $m = $this->module->get_module($m['modid']);
+  echo "<li><img src='/system/application/img/subnav/dashboard.png' alt='Dashboard'/><a href='#' onclick='goto($m[id])'>$m[name]</a><p/>$m[description]</p></li>";
+}
+echo "</ul>";
+?>
